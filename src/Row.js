afd6f2faf97c60aa7611f4ca29f7901a53c97099
Satyam <satyambhardawaj@gmail.com>
2020-10-03 13:38:35 +0530

    rollback changes

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Row.css';
+import axios from './axios';
 
-function Row() {
+const base_url = "https://image.tmdb.org/t/p/original/"
+
+function Row({ title, fetchUrl, isLargeRow }) {
+
+    const [movies, setMovies] = useState([]);
+
+    // A snippet of code which runs based on a specific condition/variable
+    useEffect(() => {
+        //if [], run once when row loads, and don't run again\
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData()
+    }, [fetchUrl]);
+
+    console.log(movies);
     return (
         <div className="row">
-            
+            <h2>{title}</h2>
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <img 
+                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                        key={movie.id}
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+                ))}
+            </div>
+
+
         </div>
     )
 }
 
-export default Row
+export default Row;
