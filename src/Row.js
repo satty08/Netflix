99c8aa23631cd85b4144da5272016e0b69593dff
Satyam <satyambhardawaj@gmail.com>
2020-10-03 13:30:48 +0530

    banner pushed

--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import axios from './axios';
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
 
     const [movies, setMovies] = useState([]);
 
@@ -26,8 +26,9 @@ function Row({ title, fetchUrl }) {
             <div className="row__posters">
                 {movies.map(movie => (
                     <img 
-                        className="row__poster"
-                        src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                        key={movie.id}
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
                 ))}
             </div>
 
