f802d1b3f84a8848bbb3fb1d165fe053a54d9402
Satyam <satyambhardawaj@gmail.com>
2020-10-06 12:26:16 +0530

    home page sections

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './Home.css'
+import './Home.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
@@ -15,9 +16,28 @@ function Home() {
                     <h1 className="banner__main">Unlimited movies, TV shows and more.</h1>
                     <h3 className="banner__secondary">Watch Anywhere. Watch Anytime.</h3>
                     <h3 className="banner__secondary">Ready to watch?</h3>
-                    <button className="home__button">Watch the Latest</button>
+                    <Link to="/latest">
+                        <button className="home__button">Watch the Latest</button>
+                    </Link>
                 </div>
             </div>
+            <div className="home__partition" />
+            <div className="home__firstBox">
+                <div className="firstBox__text">
+                    <h1>Enjoy on your TV.</h1>
+                    <h3>Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</h3>
+                </div>
+                <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" alt="" />
+            </div>
+            <div className="home__partition" />
+            <div className="home__secondBox">
+            <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" alt="" />
+                <div className="secondBox__text">
+                    <h1>Download your shows to watch offline.</h1>
+                    <h3>Save your favourites easily and always have something to watch.</h3>
+                </div>
+            </div>
+            <div className="home__partition" />
             
         </div>
     )
